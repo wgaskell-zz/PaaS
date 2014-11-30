1fdc87d02c35fb504efda8d8a671843c44189876
wgaskell <w4yneG@gmail.com>
2014-11-30 14:03:09 +0000

    Create success_page.php

---
+++ success_page.php
@@ -0,0 +1,13 @@
+<link rel="stylesheet" type="text/css" href="http://mi-linux.wlv.ac.uk/~1020198/ultrarun/css/urmain.css">
+<link rel="stylesheet" type="text/css" href="http://mi-linux.wlv.ac.uk/~1020198/ultrarun/css/form.css">
+<?php
+
+	echo "<center>";
+
+	echo "<h1> Registration Successful </h1>";
+	echo "Thank you for registering Your Username Is --> $username";
+	echo "<br /> <a href='login'> Login Here </a>";
+
+	echo "</center>";
+
+?>
