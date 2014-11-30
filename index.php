479ceca251902e4cf867f050ccdbc822638bf784
wgaskell <w4yneG@gmail.com>
2014-11-30 13:59:24 +0000

    Create index.php

---
+++ index.php
@@ -0,0 +1,91 @@
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+<title>1020198</title>
+<meta name="MobileOptimized" content="320">
+<meta name="HandheldFriendly" content="True">
+<link rel="stylesheet" href="assets/css/style.css">
+<link rel="stylesheet" href="assets/fonts/raphaelicons.css">
+<link rel="stylesheet" href="assets/css/main.css">
+<link href="http://fonts.googleapis.com/css?family=Oswald:regular" rel="stylesheet" type="text/css">
+<link href='http://fonts.googleapis.com/css?family=Junge' rel='stylesheet' type='text/css'>
+<script src="assets/js/libs/modernizr-2.5.2.min.js"></script>
+</head>
+<body>
+<header class="clearfix">
+  <div class="container"> <a id="logo" href="index.php">1020198</a>
+    <nav class="clearfix">
+      <ul role="navigation">
+        <li> <a href="index.php" class="activePage"><span class="icon"></span>Home</a> </li>
+        <li> <a href="pistemap.php"><span class="icon"></span>Piste Maps</a> </li>
+        <li> <a href="thegang.php"><span class="icon"></span>The Gang</a> </li>
+        <li> <a href="blog.php"><span class="icon"></span>Blog</a> </li>
+        <li> <a href="contact.php"><span class="icon"></span>Contact</a> </li>
+      </ul>
+    </nav>
+  </div>
+</header>
+<section role="banner">
+  <hgroup>
+    <h1>Welcome to Wayne's Snowboarding Story Board</h1>
+    <h2>A website of the places visited</h2>
+  </hgroup>
+  <article role="main" class="clearfix">
+    <div class="post">
+
+      <p>Over the last past 8 years I have travelled to numerous resorts, sampling the fine dining, conquering some of the toughest black runs and experiencing some of the apres skiing.</p>
+      <ul>
+        <li>Check out the resort reviews.</li>
+        <li>Take a look at the selection of pictures.</li>
+		<li>Meet the gang.</li>
+      </ul>
+      <a href="thegang.php" class="button left">Enter <span class="icon">:</span></a> </div>
+    <aside role="complementary"> <a href="#"><img src="assets/images/heli.jpg" alt=""></a> </aside>
+  </article>
+</section>
+<!-- // banner ends -->
+<section class="container">
+  <div class="columns">
+    <article>
+      <figure> <img src="assets/images/trees.jpg" alt="">
+        <figcaption>
+          <h3>Morzine</h3>
+          <p>Visited in 2012, Panoramic mountain views, linked to the Avoriaz resort.</p>
+        </figcaption>
+      </figure>
+    </article>
+    <article>
+      <figure> <img src="assets/images/light.jpg" alt="" class="alt-col">
+        <figcaption>
+          <h3>Borovets</h3>
+          <p>A journey into Bulgeria, the bigest resort in Bulgeria set it beautiful woodland area.</p>
+        </figcaption>
+      </figure>
+    </article>
+    <article>
+      <figure> <img src="assets/images/wayne.jpg" alt="">
+        <figcaption>
+          <h3>Andorra</h3>
+          <p>The landlocked mountainous region on Pas De La Casa.</p>
+        </figcaption>
+      </figure>
+    </article>
+    <article>
+      <figure> <img src="assets/images/alpchalet.jpg" alt="" class="alt-col">
+        <figcaption>
+          <h3>Alp Duez</h3>
+          <p>An exploration in the French Alps, and one of the longest black runs in Europe</p>
+        </figcaption>
+      </figure>
+    </article>
+  </div>
+  <!-- //.columns -->
+
+</section>
+<footer role="contentinfo">
+  <p> <span class="left">6CS007 - Project and Professionalism<a>   1020198</a> | by <a>Wayne Gaskell</a></span>
+</footer>
+<script src="assets/js/libs/jquery-1.7.1.min.js"></script>
+<script src="assets/js/script.js"></script>
+</body>
+</html>
