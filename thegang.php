929403235c692204ec4e76eb6dac97839a78a3df
wgaskell <w4yneG@gmail.com>
2014-11-30 14:03:35 +0000

    Create thegang.php

---
+++ thegang.php
@@ -0,0 +1,137 @@
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+<title>1020198</title>
+<meta name="MobileOptimized" content="320">
+<meta name="HandheldFriendly" content="True">
+<meta charset="utf-8" />
+<link rel="stylesheet" href="assets/css/style.css">
+<link rel="stylesheet" href="assets/css/main.css">
+<link rel="stylesheet" href="assets/css/imageslide.css">
+<link rel="stylesheet" href="assets/css/slide.css">
+<link href="http://fonts.googleapis.com/css?family=Oswald:regular" rel="stylesheet" type="text/css">
+<link href='http://fonts.googleapis.com/css?family=Junge' rel='stylesheet' type='text/css'>
+</head>
+<body>
+<header class="clearfix">
+  <div class="container"> <a id="logo" href="index.php">1020198</a>
+    <nav class="clearfix">
+      <ul role="navigation">
+        <li> <a href="index.php" class="activePage"><span class="icon"></span>Home</a> </li>
+        <li> <a href="pistemap.php"><span class="icon"></span>Piste Maps</a> </li>
+        <li> <a href="thegang.php"><span class="icon"></span>The Gang</a> </li>
+        <li> <a href="blog.php"><span class="icon"></span>Blog</a> </li>
+        <li> <a href="contact.php"><span class="icon"></span>Contact</a> </li>
+      </ul>
+    </nav>
+  </div>
+</header>
+<section role="banner">
+  <hgroup>
+    <h1>Welcome to Wayne's Snowboarding Story Board</h1>
+    <h2>A website of the places visited</h2>
+  </hgroup>
+  <article role="main" class="clearfix">
+    <div class="post">
+      <h2>Meet The Gang</h2>
+      <p>A selection of pictures of the gang. </p>
+      <p></p>
+    </div>
+  </article>
+</section>
+<!-- // banner ends -->
+<section class="container clearfix">
+  <aside role="complementary">
+    <h2>Adventure</h2>
+    <p>The gang are what make the holiday such an enjoyable adventure.</p>
+    <p>They provide me with many hours of laughter, watching them trying to ski and videoing some epic tumbles :-).</p>
+
+  </aside>
+  <article class="post content">
+    <h2>Slideshow Of The group</h2>
+	
+<div id="slideshow">
+    <ul class="slides">
+        <li><img src="assets/images/slide1.jpg" width="620" height="320"/></li>
+        <li><img src="assets/images/slide2.jpg" width="620" height="320"/></li>
+        <li><img src="assets/images/slide3.jpg" width="620" height="320"/></li>
+        <li><img src="assets/images/slide4.jpg" width="620" height="320"/></li>
+        <li><img src="assets/images/slide5.jpg" width="620" height="320"/></li>
+        <li><img src="assets/images/slide6.jpg" width="620" height="320"/></li>
+        <li><img src="assets/images/slide7.jpg" width="620" height="320"/></li>
+        <li><img src="assets/images/slide8.jpg" width="620" height="320"/></li>
+        <li><img src="assets/images/slide9.jpg" width="620" height="320"/></li>
+        <li><img src="assets/images/slide10.jpg" width="620" height="320"/></li>
+    </ul>
+    <span class="arrow previous"></span>
+    <span class="arrow next"></span>
+
+</div>
+
+<br><br>
+
+
+<div class="main">
+    <h2>My Favorite Scenery Shots</h2>
+			
+	<div id="slideshow" class="rs-slideshow">
+		<div class="slide-container">
+		<img src="assets/images/show1.jpg" alt="The first image in a slideshow demo." title="In The Distance" />
+		<span class="slide-caption">In The Distance</span>
+		</div>
+		<ol class="slides">
+
+                        <li>
+                                <a href="assets/images/show2.jpg"
+                                title="In The Distance">In The Distance</a>
+                        </li>
+                        <li>
+                                <a href="assets/images/show3.jpg"
+                                title="A View">A View</a>
+                        </li>
+                        <li>
+                                <a href="assets/images/show4.jpg"
+                                title="The Pistes">The Pistes</a>
+                        </li>
+                        <li>
+                                <a href="assets/images/show5.jpg"
+                                title="Nothing But Trees">Nothing But Trees</a>
+                        </li>
+                        <li>
+                                <a href="assets/images/show6.jpg"
+                                title="White Wash">White Wash</a>
+                        </li>
+                        <li>
+                                <a href="assets/images/show7.jpg"
+                                title="Snow Covered">Snow Covered</a>
+                        </li>
+                        <li>
+                                <a href="assets/images/show8.jpg"
+                                title="Mountain Peak">Mountain Peak</a>
+                        </li>
+                        <li>
+                                <a href="assets/images/show9.jpg"
+                                title="Far And Beyond">Far And Beyond</a>
+                        </li>
+                        <li>
+                                <a href="assets/images/show10.jpg"
+                                title="A Cold Day">A Cold Day</a>
+                        </li>
+
+
+</div>
+
+  </article>
+</section>
+<footer role="contentinfo">
+  <p> <span class="left">6CS007 - Project and Professionalism<a>   1020198</a> | by <a>Wayne Gaskell</a></span>
+</footer>
+<script src="assets/js/libs/jquery-1.7.1.min.js"></script>
+<script src="assets/js/libs/loadcanvas.js"></script>
+<script src="assets/js/libs/bootstrap.js"></script>
+<script src="assets/js/libs/jquery-2.1.0.min.js"></script>
+<script src="assets/js/libs/jquery.rs.slideshow.js"></script>
+<script src="assets/js/libs/modernizr.js"></script>
+
+</body>
+</html>
