309265422c17add95852ef4cc77b198d528ae716
wgaskell <w4yneG@gmail.com>
2014-11-30 14:22:18 +0000

    Create script.js

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+(function($){
+    $(document).ready(function(){
+
+	    
+		//fixing ie/older browsers
+		$('.oldies .columns article:nth-child(4n)').css('border-right', '0');
+		$('.oldies .thumb-list li:nth-child(4n)').css('margin-right','0');
+
+	
+	}); //ready
+})(jQuery);
+
+
+
+
