c680004f0e3d906ee9c28819021c61152dd2e5d1
wgaskell <w4yneG@gmail.com>
2014-12-16 21:40:00 +0000

    Update pistemap.php

--- pistemap.php
+++ pistemap.php
@@ -48,7 +48,7 @@
   <ul class="thumb-list">
     <li>
       <h3><a href="http://www.about-morzine.co.uk/wp-content/uploads/2009/09/les-gets-piste-map-large.jpg" target="_blank">Morzine</a></h3>
-      <figure> <a href="http://www.about-morzine.co.uk/wp-content/uploads/2009/09/les-gets-piste-map-large.jpg" target="_blank"><img src="assets/images/morzine.jpg" alt=""></a>
+      <figure> <a href="http://www.about-morzine.co.uk/wp-content/uploads/2009/09/les-gets-piste-map-large.jpg" target="_blank"><img src='http://s5.postimg.org/pw4pi29lj/morzine.jpg' alt=""></a>
         <figcaption>
           <p>The Morzine and Les Gets piste map gives good detail of the local area, listing piste names as well as lifts. The larger Portes du Soleil piste map gives you a broad picture of how everything is connected.</p>
 		  
@@ -57,7 +57,7 @@
     </li>
     <li>
       <h3><a href="http://www.igluski.com/images/_i57687.jpg" target="_blank">Borovets</a></h3>
-      <figure> <a href="http://www.igluski.com/images/_i57687.jpg" target="_blank"><img src="assets/images/borovets.jpg" alt=""></a>
+      <figure> <a href="http://www.igluski.com/images/_i57687.jpg" target="_blank"><img src='http://s5.postimg.org/3svan48if/borovets.jpg' alt=""></a>
         <figcaption>
           <p>Borovets is the biggest mountain resort in Bulgaria. The 40km of pistes are spread over three sectors, two of which, Markoudjika and Yastrebets, are loosely linked.</p>
         </figcaption>
@@ -65,7 +65,7 @@
     </li>
     <li>
       <h3><a href="http://www.swisswintersports.co.uk/images/chamonix_pistebig.jpg" target="_blank">Andorra</a></h3>
-      <figure> <a href="http://www.swisswintersports.co.uk/images/chamonix_pistebig.jpg" target="_blank"><img src="assets/images/pasdelacasa.jpg" alt=""></a>
+      <figure> <a href="http://www.swisswintersports.co.uk/images/chamonix_pistebig.jpg" target="_blank"><img src='http://s5.postimg.org/jxqw7tqmv/pasdelacasa.jpg' alt=""></a>
         <figcaption>
           <p>Pas de la Casa is a large, varied and high altitude ski resort, with access to 205km of piste in the Grandvalira ski area. It is lively and affordable and linked to both the nearby resorts of Soldeu and El Tarter.</p>
         </figcaption>
@@ -73,7 +73,7 @@
     </li>
     <li>
       <h3><a href="http://www.piste-maps.co.uk/Piste-Maps/France/Alpe-dHuez-Piste-Map-2012-Large.jpg" target="_blank">Alp Duez</a></h3>
-      <figure> <a href="http://www.piste-maps.co.uk/Piste-Maps/France/Alpe-dHuez-Piste-Map-2012-Large.jpg" target="_blank"><img src="assets/images/alpduez.jpg" alt=""></a>
+      <figure> <a href="http://www.piste-maps.co.uk/Piste-Maps/France/Alpe-dHuez-Piste-Map-2012-Large.jpg" target="_blank"><img src='http://s5.postimg.org/hy13oxhjr/alpduez.jpg' alt=""></a>
         <figcaption>
           <p>Alpe d'Huez is made up of a collection of high altitude resorts, or villages, all linked by a free public transport system. Alpe d'Huez itself and the wider Oisans region offers a wide range of summer and winter activities.</p>
         </figcaption>
